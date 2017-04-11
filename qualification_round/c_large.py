db2258505484c420ed73e6bf6eaee0f40f139d6b
Wesley Rocha <wesleysr1997@gmail.com>
2017-04-10 20:42:15 -0400

    Fixed (AC now)

--- qualification_round/c_large.py
+++ qualification_round/c_large.py
@@ -1,14 +1,18 @@
-from math import log2
+def log2(x):
+    if x == 0: return -2**63
+    b = 0
+    while 1<<b <= x:
+        b += 1
+    return b-1
 
 def f(n, k):
-    q = 1<<(int(log2(k))+1)
+    q = 1<<(log2(k)+1)
     p = n-k
     lo = p//q
     hi = (p+q//2)//q
     return [hi, lo]
 
-TC = int(input())
-for tc in range(TC):
+for tc in range(int(input())):
     n, k = map(int, input().split())
     ans = f(n, k)
     print('Case #{}: {} {}'.format(tc+1, ans[0], ans[1]))
