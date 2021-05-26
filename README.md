b769aa065604c8c288d21675cce454e7e5650635
Wesley Rocha <wesleysr1997@gmail.com>
2021-05-25 21:10:03 -0400

    Create script to download submissions

--- README.md
+++ README.md
@@ -1,5 +1,39 @@
-# competitive-programming
+# Competitive Programming
 
-My solutions to competitive programming problems.
+Some of my solutions to competitive programming problems.
 
-It's currently way too outdated, but I'll be updating soon. If you're looking for something specific, feel free to contact me directly.
+## Submission downloader script
+
+This repository also have an script to download multiple submissions from URI at
+once.
+
+First, create a file named `submissions.queue.txt` in the root directory. This
+file should contain a list of submission ids like this:
+
+```
+3614517
+6398988
+14804587
+```
+
+This queue will be processed during the execution of the script, removing each
+submission after downloading it's code.
+
+Now, [install yarn](https://yarnpkg.com/) if you haven't already.
+
+Install the required dependencies by running `yarn install` on the root
+directory.
+
+Run the script with `yarn start your@email.com yourPassword`, where
+`your@email.com` and `yourPassword` is your URI e-mail and password,
+respectively.
+
+The command above will start downloading files and putting them into the `uri/`
+directory.
+
+Currently, if a submission is already downloaded, the script will interrupt the
+execution. You'll need to remove this submission from the
+`submissions.queue.txt` file manually and run the script again.
+
+If the script crashes, you might want to re-run it, and it will restart from
+where it stopped.
---
+++ index.js
@@ -0,0 +1,92 @@
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+const chalk = require("chalk");
+
+const args = process.argv.slice(2);
+const [email, password] = args;
+
+const BASE_URL = "https://www.urionlinejudge.com.br/judge/pt";
+const routes = {
+  login: () => `${BASE_URL}/login`,
+  submission: (id) => `${BASE_URL}/runs/code/${id}`,
+};
+
+const SUBMISSION_QUEUE_PATH = "./submissions.queue.txt";
+
+const submissionQueueFile = fs.readFileSync(SUBMISSION_QUEUE_PATH);
+const submissionQueue = submissionQueueFile.toString().split("\n");
+console.log("Queued submissions:");
+console.log(submissionQueue);
+
+function clickAndWaitForNavigation(page, selector, clickOptions, waitOptions) {
+  return Promise.all([
+    page.waitForNavigation(waitOptions),
+    page.click(selector, clickOptions),
+  ]).then((value) => value[0]);
+}
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = (await browser.pages())[0];
+  await page.setViewport({ width: 1366, height: 768 });
+  //   page.on("console", (msg) => console.log("PAGE LOG:", msg.text()));
+
+  // login
+  await page.goto(routes.login());
+  await page.type("#email", email);
+  await page.type("#password", password);
+  await page.click("#remember-me");
+  await clickAndWaitForNavigation(page, "input.send-green");
+
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    const submission = submissionQueue.pop();
+    if (submission === undefined || submission === "") {
+      break;
+    }
+
+    const existingFiles = fs.readdirSync("./uri");
+    const submissionAlreadyExists = existingFiles.some((filename) =>
+      filename.includes(submission)
+    );
+    if (submissionAlreadyExists) {
+      console.log(
+        `Skipping submission: submission #${submission} already exists`
+      );
+      break;
+    }
+
+    console.log(`Processing submission #${submission}...`);
+
+    await page.goto(routes.submission(submission));
+    const { title, code } = await page.evaluate(() => {
+      // eslint-disable-next-line no-undef
+      const titleElement = document.querySelector(
+        "#information-code > dl > dd:nth-child(2) > a"
+      );
+      const title = titleElement && titleElement.innerText;
+
+      // eslint-disable-next-line no-undef
+      const codeElement = document.querySelector(".ace_text-layer");
+      const code = codeElement && codeElement.innerText;
+
+      return {
+        title,
+        code,
+      };
+    });
+
+    if (!title || !code) {
+      console.log("Skipping submission: couldn't get sufficient data");
+      break;
+    }
+
+    const path = `./uri/${title} - #${submission}.cpp`;
+    fs.writeFileSync(path, code);
+    console.log(`Created ${chalk.green(path)} ✅`);
+
+    fs.writeFileSync(SUBMISSION_QUEUE_PATH, submissionQueue.join("\n"));
+  }
+
+  await browser.close();
+})();
